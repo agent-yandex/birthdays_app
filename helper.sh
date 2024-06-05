5844d0a64855b7266fed9c4d8fe5741e03ea22d0
agent-yandex <fox.online12@gmail.com>
2024-06-05 22:35:53 +0300

    Add helper for easy install

--- helper.sh
+++ helper.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
+
 if [[ -z $1 ]]
 then
     echo "Usage:"
-    echo "$0 docker - run docker container"
-    echo "$0 install - generate venv and install packages"
-    echo "$0 database - run alembic migration"
-    echo "$0 run - run application"
+    echo " - $0 docker - run docker container"
+    echo " - $0 install - generate venv and install packages"
+    echo " - $0 database - run alembic migration"
+    echo " - $0 run - run application"
+    echo " - $0 all - all installation"
+    echo " - $0 clean - delete docker container"
 elif [[ $1 == "docker" ]]
 then
-    docker run -d --name fastapi_app -e POSTGRES_USER=test -e POSTGRES_PASSWORD=test -p 6233:5432 postgres:15.5
+    docker run -d --name birthday_db -e POSTGRES_USER=test -e POSTGRES_PASSWORD=test -p 6233:5432 postgres:15.5
 elif [[ $1 == "install" ]]
 then
     python3 -m venv venv
@@ -23,4 +27,17 @@ elif [[ $1 == "run" ]]
 then
     source venv/bin/activate
     uvicorn app.main:app --reload --port 5100
+elif [[ $1 == "all" ]]
+then
+    docker run -d --name birthday_db -e POSTGRES_USER=test -e POSTGRES_PASSWORD=test -p 6233:5432 postgres:15.5
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install poetry
+    poetry install --no-root
+    alembic upgrade head
+    uvicorn app.main:app --reload --port 5100
+elif [[ $1 == "clean" ]]
+then
+    docker stop birthday_db
+    docker rm birthday_db
 fi
